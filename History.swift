4635e89cd18ba88ac52789793a02967b147c9ba7
wangcan022 <32725250+wangcan022@users.noreply.github.com>
2017-11-06 02:37:40 +1100

    Create History.swift

---
+++ History.swift
@@ -0,0 +1,19 @@
+//
+//  History.swift
+//  Assignment4
+//
+//  Created by Can Wang on 30/10/17.
+//  Copyright © 2017 Can Wang. All rights reserved.
+//
+
+import UIKit
+
+// history class
+class History: NSObject {
+    
+    var moisture: String?
+    var temperature: Int?
+    var red: Double?
+    var green: Double?
+    var blue: Double?
+}
